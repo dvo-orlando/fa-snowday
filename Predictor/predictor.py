afbb7f0264b4e09ab739bf2f141952ecf9796eae
Dominic Orlando <dvo.orlando@gmail.com>
2016-10-17 23:51:59 -0400

    Acquired historical data

--- Predictor/predictor.py
+++ Predictor/predictor.py
@@ -2,7 +2,7 @@
 import json
 from pprint import pprint
 
-with open('samplejson_hourly.json') as data_file:
+with open('sample_data/samplejson_hourly.json') as data_file:
     data = json.load(data_file)
 
 
@@ -11,4 +11,24 @@ with open('samplejson_hourly.json') as data_file:
 
 
 #Example for accessing the hour of the first "hourly_forecast" object array,
-print(data["hourly_forecast"][0]["FCTTIME"]["hour"])
+    #print(data["hourly_forecast"][0]["FCTTIME"]["hour"])
+
+
+from sklearn import svm
+
+X = [[ 32 , 50 ]  ,  [22 , 80 ] , [ 50 , 20] , [33,20]]
+y = [1,1,0,0]
+
+clf = svm.SVC()
+clf.fit(X,y)
+
+
+print("Support Vectors: ")
+print(clf.support_vectors_)
+
+print("Indices of Support Vectors: ")
+print(clf.support_)
+
+
+print("Prediction: ")
+print(clf.predict([[data["hourly_forecast"][0]["temp"]["english"],data["hourly_forecast"][0]["humidity"]]]))
