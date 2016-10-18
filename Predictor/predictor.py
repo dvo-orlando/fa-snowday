a13e91b4ee6a8980d1a993eebdbd87eeb47dce7a
Dominic Orlando <dvo.orlando@gmail.com>
2016-10-18 16:38:20 -0400

    Extreme Housekeeping

--- Predictor/predictor.py
+++ Predictor/predictor.py
@@ -1,22 +1,10 @@
-#import data
-import json
-from pprint import pprint
-
-with open('sample_data/samplejson_hourly.json') as data_file:
-    data = json.load(data_file)
-
-
-#prints data
-    #pprint(data)
-
-
-#Example for accessing the hour of the first "hourly_forecast" object array,
-    #print(data["hourly_forecast"][0]["FCTTIME"]["hour"])
-
-
 from sklearn import svm
 
+
+#Training Data
 X = [[ 32 , 50 ]  ,  [22 , 80 ] , [ 50 , 20] , [33,20]]
+
+#Known "solutions"
 y = [1,1,0,0]
 
 clf = svm.SVC()
