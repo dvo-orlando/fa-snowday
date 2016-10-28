90a7fc5cea69aacbfdf893b0cc566cddebc77441
Dominic Orlando <dvo.orlando@gmail.com>
2016-10-28 16:31:23 -0400

    housekeeping

--- Predictor/predictor.py
+++ Predictor/predictor.py
@@ -1,11 +1,12 @@
 from sklearn import svm
+import numpy as np
 
-
+X = np.load('trainingdata.npy')
+print(X)
 #Training Data
-X = [[ 32 , 50 ]  ,  [22 , 80 ] , [ 50 , 20] , [33,20]]
 
 #Known "solutions"
-y = [1,1,0,0]
+y = [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
 clf = svm.SVC()
 clf.fit(X,y)
@@ -16,7 +17,3 @@ print(clf.support_vectors_)
 
 print("Indices of Support Vectors: ")
 print(clf.support_)
-
-
-print("Prediction: ")
-print(clf.predict([[data["hourly_forecast"][0]["temp"]["english"],data["hourly_forecast"][0]["humidity"]]]))
