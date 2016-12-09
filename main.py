ffba2bbb5147bac9db6e3e91be8e02032ba7acdf
Dominic Orlando <dvo.orlando@gmail.com>
2016-12-09 14:50:49 -0500

    Added prediction for 2 and 3 days after current date +Updated Dataset

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def hello():
     y = y.ravel()
 
     #Create and fit Support Vector Machine
-    clf = svm.SVC(probability=True, kernel='linear', tol=0.001)
+    clf = svm.SVC(probability=True, kernel='linear', tol=0.001, C=0.95, class_weight='balanced')
     clf.fit(X,y)
 
     try:
@@ -47,12 +47,45 @@ def hello():
         else:
             prediction[0,3] = 0
 
+
+    prediction2 = np.zeros((1,4))
+
+    dayfrom=2
+    with open("current.day") as data_file:
+        data = json.load(data_file)
+
+        prediction2[0,0] = data['daily']['data'][dayfrom]['temperatureMin']
+        prediction2[0,1] = data['daily']['data'][dayfrom]['precipProbability']
+        prediction2[0,2] = data['daily']['data'][dayfrom]['precipIntensity']
+        if 'precipAccumulation' in data['daily']['data'][dayfrom]:
+            prediction2[0,3] = data['daily']['data'][dayfrom]['precipAccumulation']
+        else:
+            prediction2[0,3] = 0
+
+    prediction3 = np.zeros((1,4))
+
+    dayfrom=3
+    with open("current.day") as data_file:
+        data = json.load(data_file)
+
+        prediction3[0,0] = data['daily']['data'][dayfrom]['temperatureMin']
+        prediction3[0,1] = data['daily']['data'][dayfrom]['precipProbability']
+        prediction3[0,2] = data['daily']['data'][dayfrom]['precipIntensity']
+        if 'precipAccumulation' in data['daily']['data'][dayfrom]:
+            prediction3[0,3] = data['daily']['data'][dayfrom]['precipAccumulation']
+        else:
+            prediction3[0,3] = 0
+
 #ORGANIZE VARIABLES TO PASS TO TEMPLATE
     resultArr = clf.predict_proba(prediction)
+    resultArr2 = clf.predict_proba(prediction2)
+    resultArr3 = clf.predict_proba(prediction3)
     final = str(resultArr.item((0,1))*100)
+    final2 = str(resultArr2.item((0,1))*100)
+    final3 = str(resultArr3.item((0,1))*100)
 
 #RENDER TEMPLATE
-    return render_template('index.html', final=final)
+    return render_template('index.html', final=final, final2=final2, final3=final3)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
